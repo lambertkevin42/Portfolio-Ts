b5e2729861429982d43d7eacf5115313ba279eee
lambertkevin42 <lambertkevin1994@gmail.com>
2023-03-02 15:23:40 +0100

    create new component ToggleCard

---
+++ src/components/ToggleCard/ToggleCard.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../ToggleCard/togglecard.scss';
+
+const ToggleCard: React.FC = () => {
+  return (
+    <div className="togglecard">
+      <h2>Design</h2>
+      <div>contenu</div>
+    </div>
+  );
+};
+
+export default ToggleCard;
