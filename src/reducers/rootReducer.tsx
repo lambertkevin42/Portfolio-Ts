819985f368c7e78a465bc0953a472352f3060321
lambertkevin42 <lambertkevin1994@gmail.com>
2023-03-13 12:34:40 +0100

    togglebutton reducer linked with rootReducer

--- src/reducers/rootReducer.tsx
+++ src/reducers/rootReducer.tsx
@@ -1,8 +1,10 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import burgerReducer from './burgerReducer';
+import toggleButtonReducer from './toggleButtonReducer';
 
 const rootReducer = combineReducers({
   burger: burgerReducer,
+  toggle: toggleButtonReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
